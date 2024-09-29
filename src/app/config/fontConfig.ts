488ce69fda504e292c4509532456d0a254af369e
rony <ronysilaban7@gmail.com>
2024-09-29 21:28:02 +0700

    fix typo

--- src/app/config/fontConfig.ts
+++ src/app/config/fontConfig.ts
@@ -1,21 +1,21 @@
 import localFont from 'next/font/local';
 
 const mulishLight = localFont({
-    src: '../fonts/mulish/Mulish-Light.tff',
+    src: '../fonts/mulish/Mulish-Light.ttf',
     weight: '400',
     style: 'normal',
     display: 'swap'
 })
 
 const mulishRegular = localFont({
-    src: '../fonts/mulish/Mulish-Regular.tff',
+    src: '../fonts/mulish/Mulish-Regular.ttf',
     weight: '400',
     style: 'normal',
     display: 'swap'
 })
 
 const mulishBold = localFont({
-    src: '../fonts/mulish/Mulish-Bold.tff',
+    src: '../fonts/mulish/Mulish-Bold.ttf',
     weight: '700',
     style: 'normal',
     display: 'swap'
