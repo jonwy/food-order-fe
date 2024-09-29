d0dc43929faaee5e69a04a1c7ac8b9d369b3d770
rony <ronysilaban7@gmail.com>
2024-09-29 21:28:11 +0700

    add 'use client'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { ThemeProvider } from '@emotion/react';
 import '../styles/global.css';
 import { mulishRegular } from './config/fontConfig';
 import theme from './theme';
+import { CssBaseline } from '@mui/material';
 
 
 export default function RootLayout({
@@ -13,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ThemeProvider theme={theme}>
+        <CssBaseline/>
         <body className={mulishRegular.className}>{children}</body>
       </ThemeProvider>
     </html>
